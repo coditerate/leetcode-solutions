04103d75e2401bf585bfc706a0ffb41e1752d18c
Khaja Mohammed <kfmohammed@gmail.com>
2025-06-12 17:57:40 +0100

    leetcode - problem 13 (roman numbers to integers)
    Signed-off-by: Khaja Mohammed <kfmohammed@gmail.com>

--- LeetCode/_0013_RomanToInteger/RomanToInteger.cs
+++ LeetCode/_0013_RomanToInteger/RomanToInteger.cs
@@ -68,31 +68,68 @@
 //    }
 //}
 
+//public class Solution
+//{
+//    public int RomanToInt(string s)
+//    {
+//        if (string.IsNullOrEmpty(s)) return 0;
+
+//        var romanNumerals = new Dictionary<char, int> { { 'I', 1 }, { 'V', 5 }, { 'X', 10 }, { 'L', 50 }, { 'C', 100 }, { 'D', 500 }, { 'M', 1000 } };
+
+//        var total = 0;
+
+//        for (var i = 0; i < s.Length; i++)
+//        {
+//            var currentValue = romanNumerals[s[i]];
+//            var nextValue = (i + 1 < s.Length) ? romanNumerals[s[i + 1]] : 0;
+
+//            if (currentValue < nextValue)
+//            {
+//                total -= currentValue;
+//            }
+//            else
+//            {
+//                total += currentValue;
+//            }
+//        }
+
+//        return total;
+//    }
+//}
+
+
 public class Solution
 {
+    private static readonly int[] RomanNumerals;
+
+    static Solution()
+    {
+        RomanNumerals = new int[89];
+        RomanNumerals['I'] = 1;
+        RomanNumerals['V'] = 5;
+        RomanNumerals['X'] = 10;
+        RomanNumerals['L'] = 50;
+        RomanNumerals['C'] = 100;
+        RomanNumerals['D'] = 500;
+        RomanNumerals['M'] = 1000;
+    }
+
     public int RomanToInt(string s)
     {
-        if (string.IsNullOrEmpty(s)) return 0;
-
-        var romanNumerals = new Dictionary<char, int> { { 'I', 1 }, { 'V', 5 }, { 'X', 10 }, { 'L', 50 }, { 'C', 100 }, { 'D', 500 }, { 'M', 1000 } };
-
-        var total = 0;
-
-        for (var i = 0; i < s.Length; i++)
+        var sum = 0;
+        var numberToCompare = 1;
+        for (var i = s.Length - 1; i >= 0; i--)
         {
-            var currentValue = romanNumerals[s[i]];
-            var nextValue = (i + 1 < s.Length) ? romanNumerals[s[i + 1]] : 0;
-
-            if (currentValue < nextValue)
+            var numCurrentVal = RomanNumerals[s[i]];
+            if (numCurrentVal >= numberToCompare)
             {
-                total -= currentValue;
+                sum += numCurrentVal;
+                numberToCompare = numCurrentVal;
             }
             else
-            {
-                total += currentValue;
-            }
+                sum -= numCurrentVal;
         }
 
-        return total;
+        return sum;
     }
 }
