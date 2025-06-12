c3969ac598592c4435bd1e27e2a830df11c0d9d7
Khaja Mohammed <kfmohammed@gmail.com>
2025-06-12 17:36:57 +0100

    leetcode - problem 13 (roman numbers to integers)
    Signed-off-by: Khaja Mohammed <kfmohammed@gmail.com>

--- LeetCode.Tests/_0013_RomanToIntegerTest.cs
+++ LeetCode.Tests/_0013_RomanToIntegerTest.cs
@@ -1,23 +1,24 @@
 using LeetCode._0013_RomanToInteger;
+using Shouldly;
 
 namespace LeetCode.Tests;
 
 public class _0013_RomanToIntegerTest
 {
-    //[Fact]
-    //public void RomanToIntegerTest()
-    //{
-    //    // Arrange
-    //    var solution = new Solution();
+    [Fact]
+    public void RomanToIntegerTest()
+    {
+        // Arrange
+        var solution = new Solution();
 
-    //    // Act
-    //    var result1 = solution.RomanToInt("III");
-    //    var result2 = solution.RomanToInt("LVIII");
-    //    var result3 = solution.RomanToInt("MCMXCIV");
+        // Act
+        var result1 = solution.RomanToInt("III");
+        var result2 = solution.RomanToInt("LVIII");
+        var result3 = solution.RomanToInt("MCMXCIV");
 
-    //    // Assert
-    //    result1.ShouldBe(3);
-    //    result2.ShouldBe(58);
-    //    result3.ShouldBe(1994);
-    //}
+        // Assert
+        result1.ShouldBe(3);
+        result2.ShouldBe(58);
+        result3.ShouldBe(1994);
+    }
 }
--- LeetCode/_0013_RomanToInteger/RomanToInteger.cs
+++ LeetCode/_0013_RomanToInteger/RomanToInteger.cs
@@ -1,9 +1,98 @@
 ﻿namespace LeetCode._0013_RomanToInteger;
 
+//public class Solution
+//{
+//    public int RomanToInt(string s)
+//    {
+//        var returnValue = 0;
+
+//        var romanNumerals = new Dictionary<char, int> { { 'I', 1 }, { 'V', 5 }, { 'X', 10 }, { 'L', 50 }, { 'C', 100 }, { 'D', 500 }, { 'M', 1000 } };
+
+//        if (string.IsNullOrEmpty(s)) return 0;
+
+//        if (s.Length == 1) return romanNumerals.GetValueOrDefault(s[0], 0);
+
+//        for (var i = 0; i < s.Length; i++)
+//        {
+//            var currentChar = s[i];
+//            var nextChar = i == s.Length - 1 ? 'Z' : s[i + 1];
+
+//            switch (currentChar)
+//            {
+//                case 'I':
+//                    if (new[] { 'V', 'X' }.Contains(nextChar))
+//                    {
+//                        returnValue += romanNumerals[nextChar] - romanNumerals[currentChar];
+//                        i++;
+//                    }
+//                    else
+//                        returnValue += romanNumerals[currentChar];
+//                    break;
+//                case 'V':
+//                    returnValue += romanNumerals[currentChar];
+//                    break;
+//                case 'X':
+//                    if (new[] { 'L', 'C' }.Contains(nextChar))
+//                    {
+//                        returnValue += romanNumerals[nextChar] - romanNumerals[currentChar];
+//                        i++;
+//                    }
+//                    else
+//                        returnValue += romanNumerals[currentChar];
+//                    break;
+//                case 'L':
+//                    returnValue += romanNumerals[currentChar];
+//                    break;
+//                case 'C':
+//                    if (new[] { 'D', 'M' }.Contains(nextChar))
+//                    {
+//                        returnValue += romanNumerals[nextChar] - romanNumerals[currentChar];
+//                        i++;
+//                    }
+//                    else
+//                        returnValue += romanNumerals[currentChar];
+//                    break;
+//                case 'D':
+//                    returnValue += romanNumerals[currentChar];
+//                    break;
+//                case 'M':
+//                    returnValue += romanNumerals[currentChar];
+//                    break;
+//                default:
+//                    returnValue = 0;
+//                    break;
+//            }
+//        }
+
+//        return returnValue;
+//    }
+//}
+
 public class Solution
 {
-    //public int RomanToInt(string s)
-    //{
+    public int RomanToInt(string s)
+    {
+        if (string.IsNullOrEmpty(s)) return 0;
 
-    //}
+        var romanNumerals = new Dictionary<char, int> { { 'I', 1 }, { 'V', 5 }, { 'X', 10 }, { 'L', 50 }, { 'C', 100 }, { 'D', 500 }, { 'M', 1000 } };
+
+        var total = 0;
+
+        for (var i = 0; i < s.Length; i++)
+        {
+            var currentValue = romanNumerals[s[i]];
+            var nextValue = (i + 1 < s.Length) ? romanNumerals[s[i + 1]] : 0;
+
+            if (currentValue < nextValue)
+            {
+                total -= currentValue;
+            }
+            else
+            {
+                total += currentValue;
+            }
+        }
+
+        return total;
+    }
 }
