56f1deaddf28f3a36a272a0af1cb00953b27fe71
Khaja Mohammed <kfmohammed@gmail.com>
2025-06-24 16:57:10 +0100

    leetcode - problem 28 (find index of first occurrence in string)

---
+++ LeetCode.Tests/_0028_FindIndexOfFirstOccurrenceInStringTest.cs
@@ -0,0 +1,38 @@
+﻿using LeetCode._0028_FindIndexOfFirstOccurrenceInString;
+using Shouldly;
+
+namespace LeetCode.Tests;
+
+public class _0028_FindIndexOfFirstOccurrenceInStringTest
+{
+    [Fact]
+    public void FindIndexOfFirstOccurrenceInStringTest()
+    {
+        // Arrange
+        var solution = new Solution();
+
+        const string hayStack1 = "sadbutsad";
+        const string needle1 = "sad";
+
+        const string hayStack2 = "leetcode";
+        const string needle2 = "leeto";
+
+        const string hayStack3 = "a";
+        const string needle3 = "a";
+
+        const string hayStack4 = "hello";
+        const string needle4 = "ll";
+
+        // Act
+        var result1 = solution.StrStr(hayStack1, needle1);
+        var result2 = solution.StrStr(hayStack2, needle2);
+        var result3 = solution.StrStr(hayStack3, needle3);
+        var result4 = solution.StrStr(hayStack4, needle4);
+
+        // Assert
+        result1.ShouldBe(0);
+        result2.ShouldBe(-1);
+        result3.ShouldBe(0);
+        result4.ShouldBe(2);
+    }
+}
---
+++ LeetCode/_0028_FindIndexOfFirstOccurrenceInString/FindIndexOfFirstOccurrenceInString.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode._0028_FindIndexOfFirstOccurrenceInString;
+
+//public class Solution
+//{
+//    public int StrStr(string haystack, string needle)
+//    {
+//        if (haystack.Length < needle.Length) return -1;
+
+//        for (var i = 0; i < haystack.Length; i++)
+//        {
+//            if (haystack[i..].StartsWith(needle)) return i;
+//        }
+
+//        return -1;
+//    }
+//}
+
+
+public class Solution
+{
+    public int StrStr(string haystack, string needle)
+    {
+        if (string.IsNullOrEmpty(needle)) return 0;
+        if (haystack.Length < needle.Length) return -1;
+
+        for (var i = 0; i <= haystack.Length - needle.Length; i++)
+        {
+            if (haystack.Substring(i, needle.Length) == needle)
+                return i;
+        }
+
+        return -1;
+    }
+}
